75440de6bba7ba60ae061c9d9646c7ee544b0c49
universe-enjoyer <universenjoyer@gmail.com>
2025-02-27 06:45:41 +0200

    Add post viewing

--- forum/routes.py
+++ forum/routes.py
@@ -1,4 +1,3 @@
-from pydoc import describe
 
 from flask import render_template, url_for
 from flask import current_app as app
@@ -8,13 +7,13 @@ from forum import db
 from forum.forms import PostForm
 from forum.models import Post
 
-
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     return render_template(
         'home.html',
         title='Home Test Page',
-        description='Default Home description'
+        description='Default Home description',
+        posts=Post.query.all()
     )
 
 @app.route('/post', methods = ['GET', 'POST'])
@@ -32,4 +31,12 @@ def create_post():
     return render_template(
         'create_post.html',
         form=form
+    )
+
+@app.route('/view/<int:post_id>', methods = ['GET', 'POST'])
+def view_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template(
+        'view_post.html',
+        post=post
     )
